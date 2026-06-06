9ee4850cecffc31e5461df4173806a1e80244fa3
shiwani <girlst75@gmail.com>
2026-06-07 01:01:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 window.onload = () => {
 console.log("Stealme Designs Website Loaded Successfully");
 };
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', async function(e){
+
+e.preventDefault();
+
+const formData = new FormData(form);
+
+await fetch(
+'https://formsubmit.co/ajax/stealmedesigns15@gmail.com',
+{
+method:'POST',
+body:formData
+}
+);
+
+document.getElementById('successPopup').style.display='flex';
+
+form.reset();
+
+});
+
+function closePopup(){
+document.getElementById('successPopup').style.display='none';
+}
